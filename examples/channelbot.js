3dcf9818452f4c697ef9c9a3fd31341ed5b5036b
David Kartnaller <david.kartnaller@gmail.com>
2018-09-01 16:30:27 +0000

    Fixed example

--- examples/channelbot.js
+++ examples/channelbot.js
@@ -32,7 +32,7 @@ ts3.on("ready", async () => {
 	//if no channelid is given list all Channel with their IDs
 	if (channelid == 0) {
 		console.log("No Channel ID given, listing all Channels available")
-		var channels = ts3.channelList()
+		var channels = await ts3.channelList()
 		channels.forEach(channel => {
 			//Logs the Channel ID and the Channel Name
 			console.log(channel.getCID(), channel.getCache().channel_name)
