62b20bf9e160a1a1691226e513752cba7a5b7cc4
David Kartnaller <david.kartnaller@gmail.com>
2018-12-12 15:08:55 +0000

    fixed invalid serverid upon connect for teamspeak servers < 3.5.0

--- TeamSpeak3.js
+++ TeamSpeak3.js
@@ -135,6 +135,8 @@ class TeamSpeak3 extends EventEmitter {
             exec.push(this.login(this._config.username, this._config.password))
         if (typeof this._config.serverport === "number" && this._build >= 1536564584)
             exec.push(this.useByPort(this._config.serverport, this._config.nickname))
+        if (typeof this._config.serverport === "number" && this._build < 1536564584)
+            exec.push(this.useByPort(this._config.serverport))
         if (typeof this._config.nickname === "string" && this._build < 1536564584)
             exec.push(this.clientUpdate({client_nickname: this._config.nickname}))
         Promise.all(exec)
