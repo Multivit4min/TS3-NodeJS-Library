237026b2446f01933a15a470d4b734d52127d07a
multivit4min <david.kartnaller@gmail.com>
2020-02-14 16:25:40 +0000

    add toJSON and toString

--- src/exception/EventError.ts
+++ src/exception/EventError.ts
@@ -2,8 +2,26 @@ export class EventError extends Error {
 
   readonly eventName: string
 
+  /**
+   * @param message error message
+   * @param eventName source event of this error
+   */
   constructor(message: string, eventName: string) {
-    super(`${message} in event "${eventName}"`)
+    super(message)
     this.eventName = eventName
+  }
+
+  /* returns a string representation for the error */
+  toString() {
+    return `${this.message} in event "${this.eventName}"`
+  }
+
+  /* returns a json representation for this error */
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      eventName: this.eventName
+    }
   }
 }
