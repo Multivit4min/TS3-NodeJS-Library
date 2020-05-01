fdd2f5acd12b593936026fbf64092610400f2ffd
Multivit4min <david.kartnaller@gmail.com>
2020-05-01 09:33:57 +0000

    Deploying to gh-pages from master 132333a40f68b95772c2d77fb4a6124f8ce9a336

--- tests/mocks/MockNode.ts
+++
@@ -1,9 +0,0 @@
-import { Abstract } from "../../src/node/Abstract"
-import { TeamSpeak } from "../../src/TeamSpeak"
-import { QueryResponse } from "../../src/types/QueryResponse"
-
-export class MockNode extends Abstract {
-  constructor(ts3: TeamSpeak, props: Partial<QueryResponse>, namespace: string) {
-    super(ts3, props, namespace)
-  }
-}
