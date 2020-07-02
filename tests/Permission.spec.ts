a470735cb33ffac12d4d3d5792bfed4375fbe9af
multivit4min <david.kartnaller@gmail.com>
2020-07-02 15:08:26 +0000

    improve test coverage

--- tests/Permission.spec.ts
+++ tests/Permission.spec.ts
@@ -1,6 +1,34 @@
+const mockExecute = jest.fn()
+const mockExecutePrio = jest.fn()
+const mockClose = jest.fn()
+const mockIsConnected = jest.fn()
+
+jest.mock("../src/transport/TeamSpeakQuery", () => {
+  const { TeamSpeakQuery } = jest.requireActual("../src/transport/TeamSpeakQuery")
+  TeamSpeakQuery.getSocket = function() {
+    return { on() {}, send() {}, sendKeepAlive() {}, close() { mockClose() }, isConnected() {} }
+  }
+  TeamSpeakQuery.prototype.execute = mockExecute
+  TeamSpeakQuery.prototype.executePrio = mockExecutePrio
+  TeamSpeakQuery.prototype.isConnected = mockIsConnected
+  return { TeamSpeakQuery }
+})
+
 import { Permission } from "../src/util/Permission"
+import { TeamSpeak } from "../src/TeamSpeak"
 
 describe("Permission", () => {
+
+  let teamspeak: TeamSpeak = new TeamSpeak({})
+
+  beforeEach(() => {
+    teamspeak = new TeamSpeak({})
+    mockClose.mockReset()
+    mockExecute.mockReset()
+    mockExecute.mockResolvedValue([])
+    mockExecutePrio.mockReset()
+    mockExecutePrio.mockResolvedValue([])
+  })
 
   describe("#getSkip()", () => {
     it("should check the correct return type", () => {
@@ -56,6 +84,8 @@ describe("Permission", () => {
       })
       perm.perm("NaN")
       expect(() => perm["getAsPermid"]()).toThrowError()
+      perm.perm(123)
+      expect(() => perm["getAsPermid"]()).toThrowError()
     })
   })
 
@@ -84,6 +114,20 @@ describe("Permission", () => {
       expect(perm["getPermName"]()).toBe("permid")
       perm.perm("foo")
       expect(perm["getPermName"]()).toBe("permsid")
+    })
+  })
+
+  describe("#remove()", () => {
+    it("should validate parameters", async () => {
+      expect.assertions(1)
+      const perm = new Permission({
+        update: "foo",
+        remove: "bar",
+        teamspeak,
+        context: {}
+      })
+      await perm.remove()
+      expect(mockExecute).toHaveBeenCalledTimes(1)
     })
   })
 
